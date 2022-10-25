ee0c79cf7a37ee1b304a35878d5a0b5817493c8a
Rafael Correia <rafaeljpc@gmail.com>
2022-10-25 12:26:39 -0300

    some script

---
+++ Assets/Scripts/Variables.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour {
+
+    int moedas;
+
+    // Start is called before the first frame update
+    void Start() {
+        moedas = 10;
+        print($"Moedas {moedas}");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
