c7c42a3f43e7952cb0a2bb40cda7a412833814a2
Rafael Correia <rafaeljpc@gmail.com>
2022-10-25 18:01:14 -0300

    finally filemode and eol fixed

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -1,14 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Variables : MonoBehaviour {
 
-    
+
     // Start is called before the first frame update
     void Start() {
-        var range = new RangeInt(30, 10);
-        
+        var range = Enumerable.Range(1, 10);
+
+        range.ToList().ForEach((i) => print($"count {i}"));
     }
 
     // Update is called once per frame
